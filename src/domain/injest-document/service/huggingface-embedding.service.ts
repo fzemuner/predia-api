1d2e4336f598d95979807c6f36f8e3bc7a85a936
Fabio Zemuner <78370034+alparFabioZemuner@users.noreply.github.com>
2025-05-25 12:17:02 -0300

    Change hf model

--- src/domain/injest-document/service/huggingface-embedding.service.ts
+++ src/domain/injest-document/service/huggingface-embedding.service.ts
@@ -3,13 +3,16 @@ import * as hf from '@huggingface/inference';
 
 @Injectable()
 export class HuggingFaceEmbeddingService {
-  private client = new hf.HfInference(process.env.HF_API_TOKEN);
+  private client = new hf.InferenceClient(process.env.HF_API_TOKEN);
 
   async embedText(text: string): Promise<number[]> {
+    console.log('HuggingFaceEmbeddingService.embedText start')
+    console.log(text)
     const response = await this.client.featureExtraction({
-      model: 'jinaai/jina-embeddings-v2-base-en',
+      model: 'sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2',//'jinaai/jina-embeddings-v2-base-en',//'jinaai/jina-embeddings-v3',
       inputs: text,
     });
+    console.log('HuggingFaceEmbeddingService.embedText end')
 
     return response as number[];
   }
